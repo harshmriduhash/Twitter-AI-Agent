07710aee1a6e1d6ba5af7a667829fc3523a8975d
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-01-30 11:50:09 +0530

    route

--- src/app/api/scraper/route.ts
+++ src/app/api/scraper/route.ts
@@ -1,20 +1,17 @@
-import { getRecentMentions} from "@/lib/TwitterBot";
+import { getRecentMentions } from "@/lib/TwitterBot";
 import { NextResponse } from "next/server";
 
+export async function GET() {
+  try {
+    const respponse = await getRecentMentions();
 
-
-export async  function  GET(){
-    try {
-        const  respponse =  await getRecentMentions();
-
-       return NextResponse.json({
-        message:respponse
-       })
-        
-    } catch (error) {
-        return NextResponse.json({
-            error:"an error  occurred",
-            msg:error
-        })
-    }
+    return NextResponse.json({
+      message: respponse,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      error: "an error  occurred",
+      msg: error,
+    });
+  }
 }
